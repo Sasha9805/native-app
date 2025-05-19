d5de4ad739f2eb7501204eb259fb04325e8428d5
Sasha Pimenov <pimenovs33@gmail.com>
2025-05-19 20:23:17 +0300

    Added Image

--- App.tsx
+++ App.tsx
@@ -1,10 +1,14 @@
-import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import { Button, StyleSheet, Text, TextInput, View, Image } from "react-native";
 
 export default function App() {
 	return (
 		<View style={styles.container}>
 			<View style={styles.content}>
-				<Text>Purpleschool</Text>
+				<Image
+					style={styles.logo}
+					source={require("./assets/logo.png")}
+					resizeMode="contain"
+				/>
 				<View style={styles.form}>
 					<TextInput style={styles.input} />
 					<TextInput style={styles.input} />
@@ -21,6 +25,7 @@ const styles = StyleSheet.create({
 		flex: 1,
 		justifyContent: "center",
 		padding: 55,
+		backgroundColor: "#16171D",
 	},
 	content: {
 		alignItems: "center",
@@ -32,5 +37,8 @@ const styles = StyleSheet.create({
 	},
 	input: {
 		backgroundColor: "#2E2D3D",
+	},
+	logo: {
+		width: 220,
 	},
 });
