ab12b6ae976063a54f5c5bff187064836e10f187
Sasha Pimenov <pimenovs33@gmail.com>
2025-05-20 20:30:50 +0300

    Added basic animation with Animated from react-native

--- shared/Button/Button.tsx
+++ shared/Button/Button.tsx
@@ -4,15 +4,43 @@ import {
 	StyleSheet,
 	Text,
 	View,
+	Animated,
 } from "react-native";
 import { Colors, Fonts, Radius } from "../tokens";
 
 export function Button({ text, ...props }: PressableProps & { text: string }) {
+	const animatedValue = new Animated.ValueXY({ x: 0, y: 0 });
+	Animated.timing(animatedValue, {
+		// spring without duration
+		toValue: {
+			x: 100,
+			y: 100,
+		},
+		duration: 2000,
+		useNativeDriver: true,
+	}).start();
 	return (
 		<Pressable {...props}>
-			<View style={styles.button}>
+			<Animated.View
+				// style={{
+				// 	...styles.button,
+				// 	transform: [
+				// 		{ translateX: animatedValue.x },
+				// 		{ translateY: animatedValue.y },
+				// 	],
+				// }}
+				style={[
+					styles.button,
+					{
+						transform: [
+							{ translateX: animatedValue.x },
+							{ translateY: animatedValue.y },
+						],
+					},
+				]}
+			>
 				<Text style={styles.text}>{text}</Text>
-			</View>
+			</Animated.View>
 		</Pressable>
 	);
 }
