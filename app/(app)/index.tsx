4492a3df569d8a748b94f404c861e74cf9a5f95d
Sasha Pimenov <pimenovs33@gmail.com>
2025-06-09 23:01:26 +0300

    Added auth atomWithStorage

--- app/(app)/index.tsx
+++ app/(app)/index.tsx
@@ -1,20 +1,9 @@
 import { useAtom } from 'jotai';
 import { Text, View } from 'react-native';
 import { profileAtom } from '../../entities/user/model/user.state';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { useEffect } from 'react';
 
 export default function MyCourses() {
 	const [profile] = useAtom(profileAtom);
-
-	useEffect(() => {
-		AsyncStorage.setItem('demo', 'test').then(async () => {
-			console.log(await AsyncStorage.getAllKeys());
-			console.log(await AsyncStorage.getItem('demo'));
-			console.log(await AsyncStorage.removeItem('demo'));
-			console.log(await AsyncStorage.getItem('demo'));
-		});
-	}, []);
 
 	return (
 		<View>
