17e274af0e3210fb5ff36bc2b309e52d2b8491cb
Sasha Pimenov <pimenovs33@gmail.com>
2025-06-11 23:27:03 +0300

    Added Redirect component to layout

--- app/(app)/index.tsx
+++ app/(app)/index.tsx
@@ -1,27 +1,9 @@
-import { useAtomValue } from 'jotai';
 import { Text, View } from 'react-native';
-import { authAtom } from '../../entities/auth/model/auth.state';
-import { useEffect } from 'react';
-import { router, useRootNavigationState, useNavigation } from 'expo-router';
 
 export default function MyCourses() {
-	const { access_token } = useAtomValue(authAtom);
-	// const state = useRootNavigationState();
-	const navigation = useNavigation();
-	const navigationState = navigation.getState();
-
-	useEffect(() => {
-		if (!navigationState?.key) {
-			return;
-		}
-		if (!access_token) {
-			router.replace('/login');
-		}
-	}, [access_token, navigationState]);
-
 	return (
 		<View>
-			<Text>Test</Text>
+			<Text>Index</Text>
 		</View>
 	);
 }
