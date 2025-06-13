b3cdd830314f81830f4f36432ffae764bc8f744a
Sasha Pimenov <pimenovs33@gmail.com>
2025-06-13 21:29:29 +0300

    Added Drawer

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -1,6 +1,8 @@
-import { Redirect, Stack } from 'expo-router';
+import { Redirect } from 'expo-router';
 import { useAtomValue } from 'jotai';
 import { authAtom } from '../../entities/auth/model/auth.state';
+import { Drawer } from 'expo-router/drawer';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export default function AppLayout() {
 	const { access_token } = useAtomValue(authAtom);
@@ -10,8 +12,10 @@ export default function AppLayout() {
 	}
 
 	return (
-		<Stack>
-			<Stack.Screen name="index" />
-		</Stack>
+		<GestureHandlerRootView style={{ flex: 1 }}>
+			<Drawer>
+				<Drawer.Screen name="index" />
+			</Drawer>
+		</GestureHandlerRootView>
 	);
 }
