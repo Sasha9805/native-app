a49eebabaf05019d60a299678243cccdefd5f131
Sasha Pimenov <pimenovs33@gmail.com>
2025-06-14 18:42:50 +0300

    Added profile fetching

--- entities/layout/ui/CustomDrawer/CustomDrawer.tsx
+++ entities/layout/ui/CustomDrawer/CustomDrawer.tsx
@@ -6,17 +6,24 @@ import { StyleSheet, View, Text, Image } from 'react-native';
 import { Colors } from '../../../../shared/tokens';
 import { CustomLink } from '../../../../shared/CustomLink/CustomLink';
 import { CloseDrawer } from '../../../../features/layout/ui/CloseDrawer/CloseDrawer';
-import { useSetAtom } from 'jotai';
+import { useAtom, useSetAtom } from 'jotai';
 import { logoutAtom } from '../../../auth/model/auth.state';
+import { loadProfileAtom } from '../../../user/model/user.state';
+import { useEffect } from 'react';
 
 export function CustomDrawer(props: DrawerContentComponentProps) {
 	const logout = useSetAtom(logoutAtom);
+	const [profile, loadProfile] = useAtom(loadProfileAtom);
+
+	useEffect(() => {
+		loadProfile();
+	}, []);
 
 	return (
 		<DrawerContentScrollView {...props} contentContainerStyle={styles.scrollView}>
 			<View style={styles.content}>
 				<CloseDrawer {...props} />
-				<Text>TEEXT</Text>
+				<Text>{profile.profile?.name}</Text>
 			</View>
 			<View style={styles.footer}>
 				<CustomLink text="Выход" onPress={() => logout()} href={'/login'} />
--- entities/user/model/user.state.ts
+++ entities/user/model/user.state.ts
@@ -1,14 +1,46 @@
 import { atom } from 'jotai';
-import { User } from './user.model';
+import { User, UserRequest } from './user.model';
+import { authAtom } from '../../auth/model/auth.state';
+import { API } from '../api/api';
+import axios, { AxiosError } from 'axios';
 
 export const profileAtom = atom<UserState>({
-	profile: {
-		id: 1,
-		name: 'Александр',
-	},
+	profile: null,
 	isLoading: false,
 	error: null,
 });
+
+export const loadProfileAtom = atom(
+	(get) => get(profileAtom),
+	async (get, set) => {
+		const { access_token } = await get(authAtom);
+		set(profileAtom, {
+			isLoading: true,
+			profile: null,
+			error: null,
+		});
+		try {
+			const { data } = await axios.get<UserRequest>(API.profile, {
+				headers: {
+					Authorization: `Bearer ${access_token}`,
+				},
+			});
+			set(profileAtom, {
+				isLoading: false,
+				profile: data.profile,
+				error: null,
+			});
+		} catch (error) {
+			if (error instanceof AxiosError) {
+				set(profileAtom, {
+					isLoading: false,
+					profile: null,
+					error: error.response?.data.message,
+				});
+			}
+		}
+	},
+);
 
 export interface UserState {
 	profile: User | null;
