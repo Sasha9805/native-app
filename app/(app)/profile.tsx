5728fa100617305f16aad216a47b1cc6b53f0538
Sasha Pimenov <pimenovs33@gmail.com>
2025-06-16 22:26:53 +0300

    Added expo-image-picker

--- app/(app)/profile.tsx
+++ app/(app)/profile.tsx
@@ -1,9 +1,26 @@
+import { useState } from 'react';
 import { Text, View } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import { Button } from '../../shared/Button/Button';
 
 export default function Profile() {
+	const [image, setImage] = useState(null);
+
+	const pickAvatar = async () => {
+		console.log('eee');
+		const result = await ImagePicker.launchCameraAsync({
+			mediaTypes: ['images'],
+			allowsEditing: true,
+			aspect: [1, 1],
+			quality: 0.5,
+		});
+		console.log(result);
+	};
+
 	return (
 		<View>
 			<Text>Profile</Text>
+			<Button text="Выбрать изображение" onPress={pickAvatar} />
 		</View>
 	);
 }
