2cf1a267907efe2d5022231f194c0794389d2021
Sasha Pimenov <pimenovs33@gmail.com>
2025-06-17 19:19:45 +0300

    Added MediaLibrary permissions and picking resources

--- app/(app)/profile.tsx
+++ app/(app)/profile.tsx
@@ -1,11 +1,18 @@
 import { useState } from 'react';
-import { Alert, Text, View } from 'react-native';
-import { launchCameraAsync, useCameraPermissions, PermissionStatus } from 'expo-image-picker';
+import { Alert, Image, Text, View } from 'react-native';
+import {
+	launchCameraAsync,
+	launchImageLibraryAsync,
+	useMediaLibraryPermissions,
+	useCameraPermissions,
+	PermissionStatus,
+} from 'expo-image-picker';
 import { Button } from '../../shared/Button/Button';
 
 export default function Profile() {
-	const [image, setImage] = useState(null);
+	const [image, setImage] = useState<string | null>(null);
 	const [cameraPermissions, requestCameraPermissions] = useCameraPermissions();
+	const [libraryPermissions, requestLibraryPermissions] = useMediaLibraryPermissions();
 
 	const verifyCameraPermissions = async () => {
 		if (!cameraPermissions || cameraPermissions?.status === PermissionStatus.UNDETERMINED) {
@@ -21,7 +28,24 @@ export default function Profile() {
 		return cameraPermissions.granted;
 	};
 
-	const pickAvatar = async () => {
+	const verifyMediaPermissions = async () => {
+		if (
+			!libraryPermissions?.accessPrivileges ||
+			libraryPermissions?.status === PermissionStatus.UNDETERMINED
+		) {
+			const res = await requestLibraryPermissions();
+			return res.granted;
+		}
+
+		if (libraryPermissions?.status === PermissionStatus.DENIED) {
+			Alert.alert('Недостаточно прав для доступа к фото');
+			return false;
+		}
+
+		return libraryPermissions.granted;
+	};
+
+	const captureAvatar = async () => {
 		const isPermissionsGranted = await verifyCameraPermissions();
 		if (!isPermissionsGranted) {
 			return;
@@ -33,12 +57,36 @@ export default function Profile() {
 			quality: 0.5,
 		});
 		console.log(result);
+		if (!result.assets) {
+			return;
+		}
+		setImage(result.assets[0].uri);
+	};
+
+	const pickAvatar = async () => {
+		const isPermissionsGranted = await verifyMediaPermissions();
+		if (!isPermissionsGranted) {
+			return;
+		}
+		const result = await launchImageLibraryAsync({
+			mediaTypes: ['images'],
+			allowsEditing: true,
+			aspect: [1, 1],
+			quality: 0.5,
+		});
+		console.log(result);
+		if (!result.assets) {
+			return;
+		}
+		setImage(result.assets[0].uri);
 	};
 
 	return (
 		<View>
 			<Text>Profile</Text>
-			<Button text="Выбрать изображение" onPress={pickAvatar} />
+			<Button text="Снять изображение" onPress={captureAvatar} />
+			<Button text="Выбрать из галереи" onPress={pickAvatar} />
+			{image && <Image source={{ uri: image, width: 100, height: 100 }} />}
 		</View>
 	);
 }
