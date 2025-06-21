d5c9368472c6d10bd960ae94153891741ae5e345
Sasha Pimenov <pimenovs33@gmail.com>
2025-06-21 16:07:25 +0300

    Added Sharing API

--- app/(app)/profile.tsx
+++ app/(app)/profile.tsx
@@ -6,10 +6,26 @@ import { Avatar } from '../../entities/user/ui/Avatar/Avatar';
 import { useAtom } from 'jotai';
 import { updateProfileAtom } from '../../entities/user/model/user.state';
 import { Button } from '../../shared/Button/Button';
+import * as Sharing from 'expo-sharing';
 
 export default function Profile() {
 	const [image, setImage] = useState<string | null>(null);
 	const [profile, updateProfile] = useAtom(updateProfileAtom);
+
+	const shareProfile = async () => {
+		const isSharingAvailable = await Sharing.isAvailableAsync();
+		if (!isSharingAvailable) {
+			return;
+		}
+
+		try {
+			await Sharing.shareAsync('https://purpleschool.ru', {
+				dialogTitle: 'Поделиться профилем',
+			});
+		} catch (e) {
+			console.error(e);
+		}
+	};
 
 	const submitProfile = () => {
 		if (!image) {
@@ -32,6 +48,7 @@ export default function Profile() {
 				<ImageUploader onUpload={setImage} onError={(e) => console.log(e)} />
 			</View>
 			<Button text="Сохранить" onPress={submitProfile} />
+			<Button text="Поделиться" onPress={shareProfile} />
 		</View>
 	);
 }
