4f386697872049da788ff9c6a920a3738b8a608b
Sasha Pimenov <pimenovs33@gmail.com>
2025-06-25 19:48:22 +0300

    Added RefreshControl to FlatList

--- app/(app)/index.tsx
+++ app/(app)/index.tsx
@@ -1,9 +1,10 @@
-import { View, StyleSheet, FlatList } from 'react-native';
+import { View, StyleSheet, FlatList, ActivityIndicator, RefreshControl, Text } from 'react-native';
 import { useAtomValue, useSetAtom } from 'jotai';
 import { courseAtom, loadCourseAtom } from '../../entities/course/model/course.state';
 import { useEffect } from 'react';
 import { CourseCard } from '../../entities/course/ui/CourseCard/CourseCard';
 import { StudentCourseDescription } from '../../entities/course/model/course.model';
+import { Colors } from '../../shared/tokens';
 
 export default function MyCourses() {
 	const { courses, isLoading, error } = useAtomValue(courseAtom);
@@ -23,8 +24,21 @@ export default function MyCourses() {
 
 	return (
 		<>
+			{isLoading && (
+				<ActivityIndicator style={styles.activity} size="large" color={Colors.primary} />
+			)}
 			{courses.length > 0 && (
 				<FlatList
+					refreshControl={
+						<RefreshControl
+							colors={[Colors.primary]}
+							progressBackgroundColor={Colors.white}
+							tintColor={Colors.primary}
+							titleColor={Colors.primary}
+							refreshing={isLoading}
+							onRefresh={loadCourses}
+						/>
+					}
 					data={courses}
 					keyExtractor={(item) => item.id.toString()}
 					renderItem={renderCourse}
@@ -37,5 +51,8 @@ export default function MyCourses() {
 const styles = StyleSheet.create({
 	item: {
 		padding: 20,
+	},
+	activity: {
+		marginTop: 30,
 	},
 });
