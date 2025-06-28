0df96b0cd16bd5fa107fc6771266e4cac5f186d4
Sasha Pimenov <pimenovs33@gmail.com>
2025-06-28 13:28:33 +0300

    Scheduled local notification

--- app/(app)/index.tsx
+++ app/(app)/index.tsx
@@ -5,6 +5,8 @@ import { useEffect } from 'react';
 import { CourseCard } from '../../widgets/course/ui/CourseCard/CourseCard';
 import { StudentCourseDescription } from '../../entities/course/model/course.model';
 import { Colors } from '../../shared/tokens';
+import { Button } from '../../shared/Button/Button';
+import * as Notifications from 'expo-notifications';
 
 export default function MyCourses() {
 	const { courses, isLoading, error } = useAtomValue(courseAtom);
@@ -22,11 +24,28 @@ export default function MyCourses() {
 		);
 	};
 
+	const scheduleNotification = () => {
+		Notifications.scheduleNotificationAsync({
+			content: {
+				title: 'Не забудь пройти курс',
+				body: 'Не забывай учиться каждый день',
+				data: {
+					success: true,
+				},
+			},
+			trigger: {
+				type: Notifications.SchedulableTriggerInputTypes.TIME_INTERVAL,
+				seconds: 5,
+			},
+		});
+	};
+
 	return (
 		<>
 			{isLoading && (
 				<ActivityIndicator style={styles.activity} size="large" color={Colors.primary} />
 			)}
+			<Button text="Напомнить" onPress={scheduleNotification} />
 			{courses.length > 0 && (
 				<FlatList
 					refreshControl={
